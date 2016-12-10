4e9c00440f721c67ec4a2fbb40ec47b296647356
Simon Casey <simon.jk.casey@gmail.com>
2016-12-10 18:27:19 +1300

    removed favicon

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var port = process.env.PORT || '3000';
 var express = require('express');
 var app = express();
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -25,8 +24,6 @@ var setupPassport = require('./passportSetup')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
