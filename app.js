e50ab09209273d7250fe182374f7ae5ebd089a57
Simon Casey <simon.jk.casey@gmail.com>
2016-12-12 13:32:09 +1300

    remove redundant views

--- app.js
+++ app.js
@@ -33,19 +33,6 @@ app.use(passport.session())
 
 app.use('/', index)
 
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 io.on('connection', function(socket) {
   console.log('connection made');
 })
